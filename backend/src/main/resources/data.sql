b3b024cc021cfc380e5034f73ecb5783fedd499b
Andre Landim <andrelandim_4@hotmail.com>
2022-02-22 12:54:25 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'tira suas dúvidas', 2, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'lives exclusivas para a turma', 3, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 1', 'Neste capítulo vamos começar', 1, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 2', 'Neste capítulo vamos continuar', 2, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 3', 'Neste capítulo vamos finalizar', 3, 'https://www.ufpb.br/cdn/contents/imagens/icones/curso.png/@@images/image.png', 1, 2);
